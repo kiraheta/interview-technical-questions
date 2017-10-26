cf9bd0d5457a4be202ce3d91450548f294467fbe
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-25 22:44:46 -0700

    code: implement O(n^2) twosum solution

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -18,3 +18,24 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
+
+# Time complexity: O(n^2)
+
+
+def two_sum(aList, target):
+    if len(aList) <= 1:
+        return aList
+    for i in range(0, len(aList) - 1):
+        for j in range(1, len(aList)):
+            if aList[i] + aList[j] == target:
+                return [i, j]
+    return [-1, -1]
+
+# TODO: implement in O(n)
+
+
+if __name__ == '__main__':
+    aList = [2, 7, 11, 15]
+    target = 9
+
+print(two_sum(aList, target))
