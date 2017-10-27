9a5f099dd51d76cd9d165ab02291a03c06318bef
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-27 14:48:14 -0700

    code: add fibonacci problem and description

---
+++ firecode/fibonacci.py
@@ -0,0 +1,18 @@
+#!bin/usr/python
+
+"""
+The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 
+34, ... The next number is found by adding up the two numbers before it.
+
+Write a recursive method fib(n) that returns the nth Fibonacci number. n is 0 
+indexed, which means that in the sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ..., 
+n == 0 should return 0 and n == 3 should return 2.
+Assume n is less than 15.
+
+Examples:
+fib(0) ==> 0
+
+fib(1) ==> 1
+
+fib(3) ==> 2
+"""
