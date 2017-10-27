441df00a6df8362e4dcf295d91f32cf9dccfea5a
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-27 13:56:51 -0700

    code: add find missing number problem and description

---
+++ firecode/findmissingnumber.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+"""
+Given an list containing 9 numbers ranging from 1 to 10, write a function to
+find the missing number. Assume you have 9 numbers between 1 to 10 and only
+one number is missing.
+
+Example:
+input_list: [1, 2, 4, 5, 6, 7, 8, 9, 10]
+find_missing_number(input_list) => 3
+"""
