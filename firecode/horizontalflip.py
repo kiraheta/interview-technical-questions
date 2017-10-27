cab381c4e39f48f9ef8666f13526d84540123e7c
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-27 15:22:56 -0700

    code: add horizontal flip problem and description

---
+++ firecode/horizontalflip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+"""  
+You are given an m x n 2D image matrix (List of Lists) where each integer 
+represents a pixel. Flip it in-place along its horizontal axis.
+
+Example:
+
+Input image :  
+              1 1
+              0 0
+Modified to :   
+              0 0
+              1 1
+"""
