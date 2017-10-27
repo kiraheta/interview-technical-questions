0d0ed1b0cfbbee613c23ba81cd483ffcfd7373e9
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-27 14:17:20 -0700

    code: add is palindrome problem and description

---
+++ firecode/ispalindrome.py
@@ -0,0 +1,20 @@
+#!/bin/usr/python
+
+"""
+A palindrome is a string or sequence of characters that reads the same backward 
+and forward. For example, "madam" is a palindrome.
+Write a function that takes in a string and returns a Boolean -> True if the 
+input string is a palindrome and False
+if it is not. An empty string is considered a palindrome. You also need to 
+account for the space character. For example, "race car" should return False 
+as read backward it is "rac ecar".
+
+Examples:
+is_palindrome("madam") -> True
+
+is_palindrome("aabb") -> False
+
+is_palindrome("race car") -> False
+
+is_palindrome("") -> True
+"""
