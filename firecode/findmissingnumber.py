15788a35de5653db45419567644faf2c51890ea7
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-27 13:59:07 -0700

    code: implement find missing number solution

--- firecode/findmissingnumber.py
+++ firecode/findmissingnumber.py
@@ -9,3 +9,22 @@ Example:
 input_list: [1, 2, 4, 5, 6, 7, 8, 9, 10]
 find_missing_number(input_list) => 3
 """
+
+
+def find_missing_number(list_numbers):
+    for num in list_numbers:
+        if num + 1 != list_numbers[num]:
+            return num + 1
+
+
+def find_missing_number(list_numbers):
+    return [i for i in range(1, 10) if i not in list_numbers][0]
+
+
+def find_missing_number(list_numbers):
+    return sum(range(11)) - sum(list_numbers)
+
+
+if __name__ == '__main__':
+    lst = [1, 2, 4, 5, 6, 7, 8, 9, 10]
+    print(find_missing_number(lst))
