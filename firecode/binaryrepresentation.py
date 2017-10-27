f17f3983c2941217fca717f6f7acc7be3eb278e2
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-27 15:13:21 -0700

    code: add binary representation problem and description

---
+++ firecode/binaryrepresentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+Write a function to compute the binary representation of a positive decimal 
+integer. The method should return a string.
+
+Example:
+dec_to_bin(6) ==> "110"
+
+dec_to_bin(5) ==> "101"
+Note : Do not use in-built bin() function.
+"""
