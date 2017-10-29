4e82bbf6a01c2db1e71e8f8dd7b2cb012958976a
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-28 23:16:27 -0700

    code: add reverse a string problem and description

---
+++ firecode/reversestring.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""  
+Write a function that takes in a string and returns the reversed version of 
+the string.
+
+Examples:
+
+reverse_string("abcde") -> "edcba"
+
+reverse_string("1") -> "1"
+
+reverse_string("") -> ""
+
+reverse_string("madam") -> "madam"
+"""
