46958bd775feb6606bd948b4593012c1a3acc03e
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-10-31 21:49:15 -0700

    code: add repeated elements problem and description

---
+++ firecode/repeatedelements.py
@@ -0,0 +1,11 @@
+"""
+Write a function - duplicate_items to find the redundant or repeated items in a 
+list and return them in sorted order.
+This method should return a list of redundant integers in ascending sorted 
+order (as illustrated below).
+
+Examples:
+duplicate_items([1, 3, 4, 2, 1]) => [1]
+
+duplicate_items([1, 3, 4, 2, 1, 2, 4]) => [1, 2, 4]
+"""
