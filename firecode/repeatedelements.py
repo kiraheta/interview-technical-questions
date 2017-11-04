f7d675fe2b24fe4139e3f41b8736153d8e3f0b7b
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-03 23:24:35 -0700

    code: implement repeated elements solution

--- firecode/repeatedelements.py
+++ firecode/repeatedelements.py
@@ -9,3 +9,21 @@ duplicate_items([1, 3, 4, 2, 1]) => [1]
 
 duplicate_items([1, 3, 4, 2, 1, 2, 4]) => [1, 2, 4]
 """
+
+
+def duplicate_items(list_numbers):
+    list_numbers.sort()
+    result = []
+    for i in range(len(list_numbers) - 1):
+        if list_numbers[i] == list_numbers[i + 1]:
+            result.append(list_numbers[i])
+    return result
+
+
+def duplicate_items(list_numbers):
+    set_list = set(list_numbers)
+    return [i for i in set_list if list_numbers.count(i) > 1]
+
+
+if __name__ == '__main__':
+    print(duplicate_items([1, 3, 4, 2, 1, 2, 4]))
