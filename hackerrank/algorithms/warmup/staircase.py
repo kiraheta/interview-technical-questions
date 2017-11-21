1e876cf944547c869920f44c04aa5195f2308739
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-20 18:03:36 -0800

    code: add staircase problem and description

---
+++ hackerrank/algorithms/warmup/staircase.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+
+"""
+Write a program that prints a right-aligned staircase
+composed of # symbols and spaces with a height and width of
+size n.
+
+Sample Input:
+6
+
+Sample Output:
+     #
+    ##
+   ###
+  ####
+ #####
+######
+"""
