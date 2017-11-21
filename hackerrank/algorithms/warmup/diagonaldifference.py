f40bb94d90b1d759a2898a9e4c7b4118b118e248
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-20 17:09:52 -0800

    code: add diagonal difference problem and description

---
+++ hackerrank/algorithms/warmup/diagonaldifference.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+
+"""
+Given a square matrix of size N X N, calculate the absolute
+difference between the sums of its diagonals. Print the
+absolute difference between the two sums of the matrix's
+diagonals as a single integer.
+
+Sample Input:
+3
+11 2 4
+4 5 6
+10 8 -12
+
+Sample Output:
+15
+"""
