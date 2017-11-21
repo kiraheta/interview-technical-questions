c99fef4b8e9865455d936dd6337590a6b85d94fc
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-20 17:16:16 -0800

    code: implement diagonal difference solution

--- hackerrank/algorithms/warmup/diagonaldifference.py
+++ hackerrank/algorithms/warmup/diagonaldifference.py
@@ -15,3 +15,18 @@ Sample Input:
 Sample Output:
 15
 """
+
+
+import sys
+
+
+lr_sum = 0
+rl_sum = 0
+n = int(input().strip())
+a = []
+for a_i in range(n):
+    a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+    lr_sum += a_t[a_i]
+    rl_sum += a_t[(n - 1) - a_i]
+result = abs(lr_sum - rl_sum)
+print(result)
