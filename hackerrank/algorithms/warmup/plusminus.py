ab8af64bcce12e7c29a7a674050fd72660777188
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-20 17:42:36 -0800

    code: add plus minus problem and description

---
+++ hackerrank/algorithms/warmup/plusminus.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+
+"""
+Given an array of integers, calculate which fraction of its
+elements are positive, which fraction of its elements are
+negative, and which fraction of its elements are zeroes,
+respectively. Print the decimal value of each fraction on a new
+line.
+
+Sample Input:
+6
+-4 3 -9 0 4 1  
+
+Sample Output:
+0.500000
+0.333333
+0.166667
+"""
