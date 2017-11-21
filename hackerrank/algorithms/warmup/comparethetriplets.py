a2dd172cfb34ffaeea5e02650c70df77aa66b2fd
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-20 16:44:35 -0800

    code: implement compare the triplets solution

--- hackerrank/algorithms/warmup/comparethetriplets.py
+++ hackerrank/algorithms/warmup/comparethetriplets.py
@@ -12,3 +12,29 @@ Sample Input:
 Sample Output:
 1 1
 """
+
+
+def solve(a0, a1, a2, b0, b1, b2):
+    alice_bob_count = [0, 0]
+    compare(alice_bob_count, a0, b0)
+    compare(alice_bob_count, a1, b1)
+    compare(alice_bob_count, a2, b2)
+    return alice_bob_count
+
+
+def compare(lst, x, y):
+    if x > y:
+        lst[0] += 1
+    if x < y:
+        lst[1] += 1
+    return lst
+
+
+if __name__ == '__main__':
+
+    a0, a1, a2 = 5, 6, 7
+    a0, a1, a2 = [int(a0), int(a1), int(a2)]
+    b0, b1, b2 = 3, 6, 10
+    b0, b1, b2 = [int(b0), int(b1), int(b2)]
+    result = solve(a0, a1, a2, b0, b1, b2)
+    print(" ".join(map(str, result)))
