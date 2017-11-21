c75b91fd0d06c70261a6d75d38f65b6df7225650
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-20 16:38:18 -0800

    code: add compare the triplets problem and description

---
+++ hackerrank/algorithms/warmup/comparethetriplets.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+"""
+Given A and B, where A = (a0, a1, a2) and B = (b0, b1, b2), write a function to
+return the comparison points, total points a person earned, as two
+space-separated integers between Alice and Bob.
+
+Sample Input:
+5 6 7
+3 6 10
+
+Sample Output:
+1 1
+"""
