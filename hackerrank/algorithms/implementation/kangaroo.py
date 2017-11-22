6fc74330f0b6887f184485973ee3a29389662f6f
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-22 14:30:27 -0800

    code: implement kangaroo solution

--- hackerrank/algorithms/implementation/kangaroo.py
+++ hackerrank/algorithms/implementation/kangaroo.py
@@ -14,3 +14,24 @@ Sample Input:
 Sample Output:
 YES
 """
+
+
+def kangaroo(x1, v1, x2, v2):
+    same_location = " "
+    if 0 <= x1 <= 10000 and 0 <= x2 <= 10000:
+        if x1 == x2 and v1 == v2:
+            same_location = "YES"
+        elif (x1 == x2 and v1 > v2) or (x1 <= x2 and v1 <= v2):
+            same_location = "NO"
+        else:
+            if ((x2 - x1) % (v1 - v2)) == 0:
+                same_location = "YES"
+            else:
+                same_location = "NO"
+    return same_location
+
+
+if __name__ == '__main__':
+    x1, v1, x2, v2 = 0, 3, 4, 2
+    result = kangaroo(x1, v1, x2, v2)
+    print(result)
