bd789b3c561ca3d9f2daee6a1128860c15ee97f2
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-22 15:08:50 -0800

    code: implement between two sets solution

--- hackerrank/algorithms/implementation/betweentwosets.py
+++ hackerrank/algorithms/implementation/betweentwosets.py
@@ -12,3 +12,28 @@ Sample Input:
 Sample Output:
 3
 """
+
+
+def getTotalX(a, b):
+    beg = max(a)
+    end = min(b)
+    inbetween = 0
+    for x in range(beg, end + 1):
+        for ai in a:
+            if x % ai != 0:
+                break
+        else:
+            for bi in b:
+                if bi % x != 0:
+                    break
+            else:
+                inbetween += 1
+    return inbetween
+
+
+if __name__ == "__main__":
+    n, m = 2, 3
+    a = [2, 4]
+    b = [16, 32, 96]
+    total = getTotalX(a, b)
+    print(total)
