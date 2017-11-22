bd8f5bd5a1cc27355fa949d78c616cdd0a361acc
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-21 23:51:46 -0800

    code: add birthday cake candles problem and description

---
+++ hackerrank/algorithms/warmup/birthdaycakecandles.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+
+"""
+Given the height for each individual candle, find and print the 
+tallest number of candles that can successfully be blown out.
+
+Sample Input:
+4
+3 2 1 3
+
+Sample Output:
+2
+"""
