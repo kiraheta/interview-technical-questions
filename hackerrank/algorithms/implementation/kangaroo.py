75ec30097866807b395327045cfc98a5d902dc11
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-22 14:23:07 -0800

    code: add kangaroo problem and description

---
+++ hackerrank/algorithms/implementation/kangaroo.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+
+"""
+Given the starting locations and movement rates for each kangaroo,
+determine if they'll ever land at the same location at the same
+time.
+
+Print YES if they can land on the same location at the same time;
+otherwise, print NO.
+
+Sample Input:
+0 3 4 2
+
+Sample Output:
+YES
+"""
