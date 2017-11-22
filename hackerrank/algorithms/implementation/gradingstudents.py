08c6b75fe35c173df7022659adb5d0004caf74c4
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-22 13:03:02 -0800

    code: add grading students problem and description

---
+++ hackerrank/algorithms/implementation/gradingstudents.py
@@ -0,0 +1,26 @@
+#!usr/bin/python3
+
+"""
+Given the initial value of grade for each of Sam's students,
+write code to automate the rounding process to the following
+rules:
+
+-If the difference between the grade and the next multiple of 5
+is less than 3, round grade up to the next multiple of 5.
+
+-If the value of grade is less than 38, no rounding occurs as the
+result will still be a failing grade
+
+Sample Input:
+4
+73
+67
+38
+33
+
+Sample Output:
+75
+67
+40
+33
+"""
