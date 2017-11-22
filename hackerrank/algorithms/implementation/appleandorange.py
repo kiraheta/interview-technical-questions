f6193dd9405e6f5af692aed1745e988523834d44
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-22 13:14:15 -0800

    code: add apple and orange problem and description

---
+++ hackerrank/algorithms/implementation/appleandorange.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+
+"""
+Given the value of d for m apples and n oranges, can you determine
+how many apples and oranges will fall on Sam's house (i.e., in
+the inclusive range [s,t])? Print the number of apples that fall
+on Sam's house as your first line of output, then print the number
+of oranges that fall on Sam's house as your second line of output.
+
+Sample Input:
+7 11
+5 15
+3 2
+-2 2 1
+5 -6
+
+Sample Output:
+1
+1
+"""
