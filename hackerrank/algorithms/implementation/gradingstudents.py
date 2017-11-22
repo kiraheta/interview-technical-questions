702678977bc6640c810394690a506857c1596aee
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-22 13:05:34 -0800

    code: implement grading students solution

--- hackerrank/algorithms/implementation/gradingstudents.py
+++ hackerrank/algorithms/implementation/gradingstudents.py
@@ -9,7 +9,7 @@ rules:
 is less than 3, round grade up to the next multiple of 5.
 
 -If the value of grade is less than 38, no rounding occurs as the
-result will still be a failing grade
+result will still be a failing grade.
 
 Sample Input:
 4
@@ -24,3 +24,20 @@ Sample Output:
 40
 33
 """
+
+
+def solve(grades):
+    for i in range(len(grades)):
+        if 0 <= grades[i] <= 100:
+            if grades[i] >= 38:
+                difference = 5 - (grades[i] % 5)
+                if difference < 3:
+                    grades[i] += difference
+    return grades
+
+
+if __name__ == '__main__':
+    n = 4
+    grades = [75, 67, 40, 33]
+    result = solve(grades)
+    print("\n".join(map(str, result)))
