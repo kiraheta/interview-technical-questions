9ecb4532dc6e3f6bac2b41b300148e8c66cb2b5d
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-22 12:27:25 -0800

    code: implement time conversion solution

--- hackerrank/algorithms/warmup/timeconversion.py
+++ hackerrank/algorithms/warmup/timeconversion.py
@@ -10,3 +10,27 @@ Sample Input:
 Sample Output:
 19:05:45
 """
+
+
+def timeConversion(s):
+    hours = s[:2]
+    mins = s[3:5]
+    am_pm = s[8:10]
+    if am_pm == "AM":
+        if hours == "12":
+            military_time = "00" + s[2:8]
+        else:
+            military_time = s[:8]
+    else:
+        if hours != "12":
+            hours = int(hours) + 12
+            military_time = str(hours) + s[2:8]
+        else:
+            military_time = s[:8]
+    return military_time
+
+
+if __name__ == '__main__':
+    s = "07:05:45PM"
+    result = timeConversion(s)
+    print(result)
