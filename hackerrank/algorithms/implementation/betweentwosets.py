c9af74ed4540da0846e08e5d3172af8962a31c59
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-22 15:06:28 -0800

    code: add between two sets problem and description

---
+++ hackerrank/algorithms/implementation/betweentwosets.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+"""
+Given A and B, find and print the number of integers (i.e.,
+possible x's) that are between the two sets.
+
+Sample Input:
+2 3
+2 4
+16 32 96
+
+Sample Output:
+3
+"""
