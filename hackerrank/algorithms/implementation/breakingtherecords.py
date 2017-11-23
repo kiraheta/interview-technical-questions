f13a0561fbabd832f98514519e7fe18af4db3bbf
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-23 01:24:05 -0800

    code: add breaking the records problem and description

---
+++ hackerrank/algorithms/implementation/breakingtherecords.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+"""
+Given Maria's array of scores for a season of n games, find and
+print the number of times she breaks her record for most and
+least points scored during the season.
+
+Sample Input:
+9
+10 5 20 20 4 5 2 25 1
+
+Sample Output:
+2 4
+"""
