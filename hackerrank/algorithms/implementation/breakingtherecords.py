e5fc8b236bd70f56c080445e2da0dab65db76ca9
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-23 01:34:42 -0800

    code: implement breaking the records solution

--- hackerrank/algorithms/implementation/breakingtherecords.py
+++ hackerrank/algorithms/implementation/breakingtherecords.py
@@ -12,3 +12,22 @@ Sample Input:
 Sample Output:
 2 4
 """
+
+
+def getRecord(s):
+    min_score = max_score = s[0]
+    min_count = max_count = 0
+    for score in s:
+        if min_score < score:
+            min_score = score
+            min_count += 1
+        if max_score > score:
+            max_score = score
+            max_count += 1
+    return (min_count, max_count)
+
+
+n = 9
+s = [10, 5, 20, 20, 4, 5, 2, 25, 1]
+result = getRecord(s)
+print(" ".join(map(str, result)))
