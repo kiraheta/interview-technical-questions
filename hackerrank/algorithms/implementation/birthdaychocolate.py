8a1f2f3be6c22b02ff4d6a01359fc81c91c6db2f
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-23 01:42:16 -0800

    code: add birthday chocolate problem and description

---
+++ hackerrank/algorithms/implementation/birthdaychocolate.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+
+"""
+Given m, d, and the sequence of integers written on each square
+of Lily's chocolate bar, find the different ways that Lily can
+break off a piece of chocolate to give to Ron where the sum of m
+consecutive squares is equal to d.
+
+Sample Input:
+5
+1 2 1 3 2
+3 2
+
+Sample Output:
+2
+"""
