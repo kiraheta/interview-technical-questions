8e394d605fb0a0984ccfcf31ac437fd7ba4e21d1
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-23 21:30:55 -0800

    code: implement birthday chocolate solution

--- hackerrank/algorithms/implementation/birthdaychocolate.py
+++ hackerrank/algorithms/implementation/birthdaychocolate.py
@@ -14,3 +14,18 @@ Sample Input:
 Sample Output:
 2
 """
+
+
+def solve(n, s, d, m):
+    count = 0
+    for i in range(n - m + 1):
+        count += int(sum(s[i:i + m]) == d)
+    return count
+
+
+if __name__ == '__main__':
+    n = 5
+    s = [1, 2, 1, 3, 2]
+    d, m = 3, 2
+    result = solve(n, s, d, m)
+    print(result)
