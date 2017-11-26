b62e8ec1e62ebdd4409e24694fd0ea3289496a6b
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-25 16:31:29 -0800

    code: add divisible sum pairs problem and description

---
+++ hackerrank/algorithms/implementation/divisiblesumpairs.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+"""
+Given an array of n integers, a0, a1,...an-1 and a positive
+integer k, find and print the number of (i, j) pairs where
+i < j  and ai + aj is divisible by k.
+
+Sample Input:
+6 3
+1 3 2 6 1 2
+
+Sample Output:
+5
+"""
