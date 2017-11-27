a0ad68778f0d102f12268539a2a7ef5031aa0436
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-26 22:50:00 -0800

    code: implement divisible sum pairs solution

--- hackerrank/algorithms/implementation/divisiblesumpairs.py
+++ hackerrank/algorithms/implementation/divisiblesumpairs.py
@@ -12,3 +12,19 @@ Sample Input:
 Sample Output:
 5
 """
+
+
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+    return count
+
+
+if __name__ == '__main__':
+    n, k = 6, 3
+    ar = [1, 3, 2, 6, 1, 2]
+    result = divisibleSumPairs(n, k, ar)
+    print(result)
