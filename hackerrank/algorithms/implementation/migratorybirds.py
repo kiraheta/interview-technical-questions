af22120212d8fd2e6c90b807fe21fc3707f2a3ea
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-28 21:00:57 -0800

    code: add migratory birds problem and description

---
+++ hackerrank/algorithms/implementation/migratorybirds.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+
+"""
+Given an array of n integers where each integer describes the type
+of a bird in the flock, find and print the type number of the most
+common bird. If two or more types of birds are equally common,
+choose the type with the smallest ID number.
+
+Sample Input:
+6
+1 4 4 4 5 3
+
+Sample Output:
+4
+"""
