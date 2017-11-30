0099ba2c3bde3190a7ebcd49dc2ecd082a48b333
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-30 15:34:13 -0800

    code: implement day of the programmer solution

--- hackerrank/algorithms/implementation/dayoftheprogrammer.py
+++ hackerrank/algorithms/implementation/dayoftheprogrammer.py
@@ -12,3 +12,20 @@ Sample Input:
 Sample Output:
 13.09.2017
 """
+
+
+def solve(year):
+    if year == 1918:
+        date = "26.09.1918"
+    else:
+        if (year < 1918 and year % 4 == 0) or (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+            date = "12.09." + str(year)
+        else:
+            date = "13.09." + str(year)
+    return date
+
+
+if __name__ == '__main__':
+    year = 2017
+    result = solve(year)
+    print(result)
