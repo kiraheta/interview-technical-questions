ac7fade461d19e0cbc90c28b8d183d927f38150c
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-11-29 23:27:34 -0800

    code: add day of the programmer problem and description

---
+++ hackerrank/algorithms/implementation/dayoftheprogrammer.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+"""
+Given a year, y, find the date of the 256th day of that year
+according to the official Russian calendar during that year. Then
+print it in the format dd.mm.yyyy, where dd is the two-digit day,
+mm is the two-digit month, and yyyy is y.
+
+Sample Input:
+2017
+
+Sample Output:
+13.09.2017
+"""
