15c7538b6622f1b03c421e858c92c15357062bb3
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-12-02 23:22:02 -0800

    code: add bon appetit problem and description

---
+++ hackerrank/algorithms/implementation/bonappetit.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+
+"""
+Given n, k the cost of each of the n items, and the total amount
+of money that Brian charged Anna for her portion of the bill. If
+the bill is fairly split, print Bon Appetit; otherwise, print the
+amount of money that Brian must refund to Anna.
+
+Sample Input:
+4 1
+3 10 2 9
+12
+
+Sample Output:
+5
+"""
