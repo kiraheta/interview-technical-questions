c2f37fbf8aef547566dce0a0452088bb1fbb3c7f
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-12-03 18:24:14 -0800

    code: implement bon appetit solution

--- hackerrank/algorithms/implementation/bonappetit.py
+++ hackerrank/algorithms/implementation/bonappetit.py
@@ -14,3 +14,19 @@ Sample Input:
 Sample Output:
 5
 """
+
+def bonAppetit(n, k, b, ar):
+    actual = (sum(ar) - ar[k]) // 2
+    if b == actual:
+        result = "Bon Appetit"
+    else:
+        diff = b - actual
+        result = diff
+    return result
+
+if __name__ == '__main__':
+    n, k = 4, 1
+    ar = [3, 10, 2, 9]
+    b = 12
+    result = bonAppetit(n, k, b, ar)
+    print(result)
