7b942273ff079595074a7b5861996ae863daacfa
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-12-06 03:07:08 -0800

    code: implement sock merchant solution

--- hackerrank/algorithms/implementation/sockmerchant.py
+++ hackerrank/algorithms/implementation/sockmerchant.py
@@ -11,3 +11,23 @@ Sample Input:
 Sample Output:
 3
 """
+
+
+def solve(arr):
+    pairs = {}
+    count = 0
+    for item in arr:
+        if item in pairs.keys():
+            pairs[item] += 1
+        else:
+            pairs[item] = 1
+    for k, v in pairs.items():
+        if v >= 2:
+            count += v // 2
+    return count
+
+
+if __name__ == '__main__':
+    arr = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+    result = solve(arr)
+    print(result)
