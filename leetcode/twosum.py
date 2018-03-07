e02317eeb8722a9c488a7be23e20abb0f1ce62ff
kiraheta <iraheta.kenny@gmail.com>
2018-03-06 18:14:03 -0800

    code: implement two sum 4 solution

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -60,7 +60,36 @@ def two_sum_3(aList, target):
     return [-1, -1]
 
 
+def two_sum_4(S, k):
+    """
+    Sorts array. The walks two counters inwards from the ends of the array, at
+    each point looking at their sum. If it equals k, then we're done. If it
+    exceeds k, then move right pointer left. If less than k, then move left
+    pointer right.
+
+    Run time is O(n log n)
+    """
+    if len(S) < 2:
+        return False
+
+    S.sort()
+
+    left = 0
+    right = len(S) - 1
+
+    while left < right:
+        sum = S[left] + S[right]
+        if sum == k:
+            return [left, right]
+        elif sum < k:
+            left += 1
+        else:
+            right -= 1
+
+    return [-1, -1]
+
+
 if __name__ == '__main__':
     aList = [2, 7, 11, 15]
-    target = 9
-    print(two_sum_3(aList, target))
+    target = 26
+    print(two_sum_4(aList, target))
